9cc7b0c056f48c474e1c04698a9a960e46005eb3
Matheus-Branco <jeremiasmatheus007@gmail.com>
2024-10-30 13:58:57 -0300

    mudificações em despesaProfile, databaseController, despesaViewModel e tarefaViewModel

--- server/eAgenda.WebApi/Config/AutomapperConfig/DespesaProfile.cs
+++ server/eAgenda.WebApi/Config/AutomapperConfig/DespesaProfile.cs
@@ -16,9 +16,7 @@ namespace eAgenda.WebApi.Config.AutoMapperConfig
             CreateMap<Despesa, ListarDespesaViewModel>()
                 .ForMember(destino => destino.FormaPagamento, opt => opt.MapFrom(origem => origem.FormaPagamento.GetDescription()));
 
-            CreateMap<Despesa, VisualizarDespesaViewModel>()
-                .ForMember(destino => destino.FormaPagamento, opt => opt.MapFrom(origem => origem.FormaPagamento.GetDescription()))
-                .ForMember(destino => destino.Categorias, opt => opt.MapFrom(origem => origem.Categorias.Select(x => x.Titulo)));
+            CreateMap<Despesa, VisualizarDespesaViewModel>();
         }
     }
 
